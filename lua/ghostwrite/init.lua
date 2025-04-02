59b2408f7cd3321319f382109ec7de097b766653
AdamFrenzen <adam.frenzen@gmail.com>
2025-04-01 23:01:53 -0600

    Display mock response in inline chat

--- lua/ghostwrite/init.lua
+++ lua/ghostwrite/init.lua
@@ -11,6 +11,11 @@ function M.setup()
 		noremap = true,
 		silent = true,
 	})
+	vim.keymap.set("n", "<leader>Gr", "<cmd>ReloadGhostwrite<cr>", {
+		desc = "Ghostwrite: Inline Chat",
+		noremap = true,
+		silent = true,
+	})
 end
 
 -- Development: hot-reload the module
@@ -68,13 +73,44 @@ function M.open_popup()
 	vim.keymap.set("n", "o", "<Nop>", { buffer = popup.bufnr, noremap = true })
 	vim.keymap.set("n", "O", "<Nop>", { buffer = popup.bufnr, noremap = true })
 
-	-- Handle <Enter>: print input, then close popup
 	vim.keymap.set("i", "<CR>", function()
 		local line = vim.api.nvim_buf_get_lines(popup.bufnr, 0, 1, false)[1] or ""
-		print("💬", line)
-		popup:unmount()
-	end, { buffer = popup.bufnr, noremap = true })
 
+		-- Clear the input line (or keep it if you want to show it)
+		vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, {
+			"👤 " .. line,
+			"", -- spacer line
+		})
+
+		-- Resize popup to fit response
+		local response_lines = {
+			"🤖 Sure! Here's what I did:",
+			" - Removed the unused import.",
+			" - Wrapped the call in a try/catch.",
+		}
+
+		local function append_line(i)
+			if i > #response_lines then
+				return
+			end
+
+			local current_lines = vim.api.nvim_buf_get_lines(popup.bufnr, 0, -1, false)
+			table.insert(current_lines, response_lines[i])
+			vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, current_lines)
+
+			-- Dynamically grow popup
+			vim.api.nvim_win_set_height(popup.winid, #current_lines)
+
+			vim.defer_fn(function()
+				append_line(i + 1)
+			end, 100)
+		end
+
+		append_line(1)
+
+		-- Exit insert mode (optional)
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+	end, { buffer = popup.bufnr, noremap = true })
 	-- Close on <Esc>
 	popup:map("n", "<Esc>", function()
 		popup:unmount()
