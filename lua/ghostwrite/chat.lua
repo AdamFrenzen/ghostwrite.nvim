d0d7d68ef990eccac7c9800c8826c079abcce0ad
AdamFrenzen <adam.frenzen@gmail.com>
2025-04-03 20:12:36 -0600

    feat: add toolbar buttons with cursor control and implement focus hooks

--- lua/ghostwrite/chat.lua
+++ lua/ghostwrite/chat.lua
@@ -57,40 +57,72 @@ function M.open()
 	local output_popup = ghostwrite_output_popup
 	local layout = ghostwrite_chat_panel
 
-	local panes = { output_popup, toolbar_popup, input_popup }
+	local panes = {
+		{ popup = output_popup },
+		{
+			popup = toolbar_popup,
+			on_focus = function(winid)
+				ChatToolbar.on_focus(winid)
+			end,
+			on_unfocus = ChatToolbar.on_unfocus,
+		},
+		{ popup = input_popup },
+	}
 	-- start with the input pane focused
 	local current_focus = 3
 
-	local function set_focus(focus)
-		local pane = panes[focus]
-
-		if not vim.bo[pane.bufnr].modifiable then
-			vim.bo[pane.bufnr].modifiable = true
-			vim.api.nvim_set_current_win(pane.winid)
-			vim.bo[pane.bufnr].modifiable = false
+	local function safely_focus_win(winid, bufnr)
+		if not vim.bo[bufnr].modifiable then
+			vim.bo[bufnr].modifiable = true
+			vim.api.nvim_set_current_win(winid)
+			vim.bo[bufnr].modifiable = false
 		else
-			vim.api.nvim_set_current_win(pane.winid)
+			vim.api.nvim_set_current_win(winid)
 		end
 	end
 
-	-- toggle between output, toolbar, and input
-	local function focus_up()
-		if current_focus == 1 then
-			current_focus = 3
-		else
-			current_focus = current_focus - 1
+	local function set_focus(target_focus)
+		local pane = panes[target_focus]
+
+		-- unfocus previous pane
+		if panes[current_focus].on_unfocus then
+			panes[current_focus].on_unfocus()
 		end
 
-		set_focus(current_focus)
+		-- set win to the popup
+		safely_focus_win(pane.popup.winid, pane.popup.bufnr)
+
+		-- trigger focus method
+		if pane.on_focus then
+			pane.on_focus(pane.popup.winid)
+		end
+
+		-- update focus
+		current_focus = target_focus
+	end
+	-- init focus
+	set_focus(current_focus)
+
+	local function focus_up()
+		local new_focus = nil
+		if current_focus == 1 then
+			new_focus = 3
+		else
+			new_focus = current_focus - 1
+		end
+
+		set_focus(new_focus)
 	end
 
 	local function focus_down()
+		local new_focus = nil
 		if current_focus == 3 then
-			current_focus = 1
+			new_focus = 1
 		else
-			current_focus = current_focus + 1
+			new_focus = current_focus + 1
 		end
-		set_focus(current_focus)
+
+		set_focus(new_focus)
 	end
 
 	local function close()
@@ -100,11 +132,11 @@ function M.open()
 	-- set shared keys for all popups
 	for _, popup in ipairs(panes) do
 		-- pane movement
-		popup:map("n", "<Up>", focus_up, M.default_bind_opts)
-		popup:map("n", "<Down>", focus_down, M.default_bind_opts)
-		popup:map("n", "<Tab>", focus_up, M.default_bind_opts)
+		popup.popup:map("n", "<Up>", focus_up, M.default_bind_opts)
+		popup.popup:map("n", "<Down>", focus_down, M.default_bind_opts)
+		popup.popup:map("n", "<Tab>", focus_up, M.default_bind_opts)
 		-- exit
-		popup:map("n", "<Esc>", close, M.default_bind_opts)
+		popup.popup:map("n", "<Esc>", close, M.default_bind_opts)
 	end
 end
 
--- lua/ghostwrite/chat_toolbar.lua
+++ lua/ghostwrite/chat_toolbar.lua
@@ -17,6 +17,7 @@ function M.create(chat_panel)
 
 	local template_button = {
 		label = "[ templates]",
+		action_is_delete = false,
 		action = function()
 			print("close")
 		end,
@@ -24,6 +25,7 @@ function M.create(chat_panel)
 
 	local context_button = {
 		label = "[ context]",
+		action_is_delete = false,
 		action = function()
 			print("context")
 		end,
@@ -31,18 +33,31 @@ function M.create(chat_panel)
 
 	local context_buttons = {
 		{
-			label = "main.rs:10-20", -- 13 len
+			label = "main.rs:10-20",
+			action_is_delete = true,
 			action = function()
+				-- eventually remove the file from context here
 				print("main.rs")
 			end,
 		},
 		{
-			label = "init.lua", -- 8 len
+			label = "init.lua",
+			action_is_delete = true,
 			action = function()
+				-- eventually remove the file from context here
 				print("init.lua")
 			end,
 		},
 	}
+
+	M.all_buttons = {}
+	-- add single buttons
+	table.insert(M.all_buttons, template_button)
+	table.insert(M.all_buttons, context_button)
+	-- add buttons from context_buttons
+	for _, button in ipairs(context_buttons) do
+		table.insert(M.all_buttons, button)
+	end
 
 	local function build_line()
 		local line_parts = {}
@@ -50,26 +65,26 @@ function M.create(chat_panel)
 
 		-- add [templates] button
 		table.insert(line_parts, " " .. template_button.label .. " ")
-		template_button.start_col = 1 -- leading padding space
-		visual_len = vim.fn.strdisplaywidth(table.concat(line_parts))
-		template_button.end_col = visual_len - 1 -- minus the trailing space
+		template_button.start_col = #" " -- leading padding space
+		visual_len = #table.concat(line_parts)
+		template_button.end_col = visual_len - #" " -- minus the trailing space bytes
 
 		-- add [context] button
 		table.insert(line_parts, context_button.label .. ": ")
 		context_button.start_col = visual_len
-		visual_len = vim.fn.strdisplaywidth(table.concat(line_parts))
-		context_button.end_col = visual_len - 2 -- again, accounting for ": "
+		visual_len = #table.concat(line_parts)
+		context_button.end_col = visual_len - #": " -- again, accounting for ": " bytes
 
 		-- add context buttons
 		for i, btn in ipairs(context_buttons) do
 			if i > 1 then
 				table.insert(line_parts, ", ")
-				visual_len = visual_len + vim.fn.strdisplaywidth(", ")
+				visual_len = #table.concat(line_parts)
 			end
 
 			btn.start_col = visual_len
 			table.insert(line_parts, btn.label)
-			visual_len = vim.fn.strdisplaywidth(table.concat(line_parts))
+			visual_len = #table.concat(line_parts)
 			btn.end_col = visual_len
 		end
 
@@ -77,18 +92,86 @@ function M.create(chat_panel)
 	end
 
 	vim.bo[toolbar_popup.bufnr].modifiable = true
-	vim.api.nvim_buf_set_lines(toolbar_popup.bufnr, 0, 1, false, { build_line() })
-	vim.highlight.range(
-		toolbar_popup.bufnr,
-		vim.api.nvim_create_namespace("toolbar_popup_style"),
-		"FloatBorder", -- make text same color as the nui borders
-		{ 0, 0 }, -- start: line 0, col 0
-		{ 0, -1 }, -- end: line 0, col -1 (to end of line)
-		{ inclusive = true }
-	)
-	vim.bo[toolbar_popup.bufnr].modifiable = false
 
+	-- add the toolbar text and change the text color to blend in
+	local function draw_toolbar()
+		vim.api.nvim_buf_set_lines(toolbar_popup.bufnr, 0, 1, false, { build_line() })
+		-- highlight the toolbar
+		vim.highlight.range(
+			toolbar_popup.bufnr,
+			vim.api.nvim_create_namespace("ghostwrite_toolbar_text"),
+			"FloatBorder", -- make text same color as the nui borders
+			{ 0, 0 }, -- start: line 0, col 0
+			{ 0, -1 }, -- end: line 0, col -1 (to end of line)
+			{ inclusive = true }
+		)
+	end
+	draw_toolbar()
+
+	M.draw_toolbar = draw_toolbar
+	M.toolbar_popup = toolbar_popup
 	return toolbar_popup
+end
+
+function M.on_focus(winid)
+	-- move cursor onto button
+	local function set_cursor(index)
+		if winid then
+			vim.api.nvim_win_set_cursor(winid, { 1, M.all_buttons[index].start_col })
+		end
+	end
+
+	-- highlight selected button
+	vim.api.nvim_set_hl(0, "GhostwriteRedBackground", { bg = "#8b2c2c" })
+	local ns_id = vim.api.nvim_create_namespace("ghostwrite_toolbar_text_select")
+	local selected_button_index = 1
+	local function highlight_button(index)
+		local button = M.all_buttons[index]
+		vim.highlight.range(
+			M.toolbar_popup.bufnr,
+			ns_id,
+			button.action_is_delete and "GhostwriteRedBackground" or "Visual",
+			{ 0, button.start_col },
+			{ 0, button.end_col },
+			{ inclusive = false }
+		)
+	end
+
+	local function redraw_toolbar(index)
+		M.draw_toolbar()
+		set_cursor(index)
+		highlight_button(index)
+	end
+	redraw_toolbar(selected_button_index)
+
+	local function select_left()
+		if selected_button_index > 1 then
+			selected_button_index = selected_button_index - 1
+		end
+		redraw_toolbar(selected_button_index)
+	end
+
+	local function select_right()
+		if selected_button_index < 4 then
+			selected_button_index = selected_button_index + 1
+		end
+		redraw_toolbar(selected_button_index)
+	end
+
+	local function select()
+		M.all_buttons[selected_button_index].action()
+	end
+
+	M.toolbar_popup:map("n", "<Left>", select_left, M.default_bind_opts)
+	M.toolbar_popup:map("n", "h", select_left, M.default_bind_opts)
+	M.toolbar_popup:map("n", "<Right>", select_right, M.default_bind_opts)
+	M.toolbar_popup:map("n", "l", select_right, M.default_bind_opts)
+
+	M.toolbar_popup:map("n", "<Cr>", select, M.default_bind_opts)
+end
+
+function M.on_unfocus()
+	M.draw_toolbar()
 end
 
 return M
