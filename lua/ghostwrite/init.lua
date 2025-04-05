349092aea055c6178cc1971ca488c8ddda28b299
AdamFrenzen <adam.frenzen@gmail.com>
2025-04-05 13:23:33 -0600

    refactor: apply keybind config opts to init commands

--- lua/ghostwrite/init.lua
+++ lua/ghostwrite/init.lua
@@ -8,24 +8,37 @@ function M.setup(opts)
 	vim.api.nvim_create_user_command("GhostwriteInline", require("ghostwrite.inline").open, {})
 	vim.api.nvim_create_user_command("GhostwriteChat", require("ghostwrite.chat").open, {})
 
+	local bind_opts = config.get().default_bind_opts
+
 	-- development command
-	vim.keymap.set("n", "<leader>Gr", "<cmd>ReloadGhostwrite<cr>", {
-		desc = "Ghostwrite: Reload plugin",
-		noremap = true,
-		silent = true,
-	})
+	vim.keymap.set(
+		"n",
+		"<leader>Gr",
+		"<cmd>ReloadGhostwrite<cr>",
+		vim.tbl_extend("force", bind_opts, {
+			desc = "Ghostwrite: Reload plugin",
+		})
+	)
+
 	-- inline chat
-	vim.keymap.set("n", "<leader>Gi", "<cmd>GhostwriteInline<cr>", {
-		desc = "Ghostwrite: Inline Chat",
-		noremap = true,
-		silent = true,
-	})
+	vim.keymap.set(
+		"n",
+		"<leader>Gi",
+		"<cmd>GhostwriteInline<cr>",
+		vim.tbl_extend("force", bind_opts, {
+			desc = "Ghostwrite: Inline Chat",
+		})
+	)
+
 	-- chat panel
-	vim.keymap.set("n", "<leader>Gc", "<cmd>GhostwriteChat<cr>", {
-		desc = "Ghostwrite: Reload plugin",
-		noremap = true,
-		silent = true,
-	})
+	vim.keymap.set(
+		"n",
+		"<leader>Gc",
+		"<cmd>GhostwriteChat<cr>",
+		vim.tbl_extend("force", bind_opts, {
+			desc = "Ghostwrite: Chat Panel",
+		})
+	)
 end
 
 function _G.ReloadGhostwrite()
