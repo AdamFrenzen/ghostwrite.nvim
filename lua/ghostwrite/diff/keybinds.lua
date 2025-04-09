577f65e45babb56f4ea9d0e1ee0c3ab546d6a52f
AdamFrenzen <adam.frenzen@gmail.com>
2025-04-08 19:13:07 -0600

    feat(diff): add diff helper virtual line

--- lua/ghostwrite/diff/keybinds.lua
+++ lua/ghostwrite/diff/keybinds.lua
@@ -16,7 +16,6 @@ function M.set_keybind()
 		buffer = 0,
 		desc = "Accept diff (default: Y, overrides 'yank to end of line')",
 	})
-
 	vim.keymap.set("n", config.diff_keybinds.reject_key, reject, {
 		buffer = 0,
 		desc = "Reject diff (default: N, overrides 'previous search result')",
@@ -44,22 +43,49 @@ function M.remove_keybind()
 	end
 end
 
-function M.attach_line_listener(target_line_number)
+function M.attach_line_listener(target_line_number, bufnr, ns_id)
 	-- Create or reuse a unique autocmd group
 	local group = vim.api.nvim_create_augroup("GhostwriteDiffWatcher", { clear = true })
 	local is_keybind_active = false
+
+	local function entered_line()
+		-- Render a helper to show how to accept/reject
+		M.diff_tag = vim.api.nvim_buf_set_extmark(bufnr, ns_id, target_line_number, 0, {
+			virt_text = {
+				{ " ", "Normal" },
+				{ "[󰊠 -+] Y/N", "GhostwriteDiffHelper" },
+			},
+			virt_text_pos = "eol", -- aligns the virtual text at the end of the line
+		})
+
+		M.set_keybind()
+		is_keybind_active = true
+	end
+
+	local function exited_line()
+		if M.diff_tag then
+			vim.api.nvim_buf_del_extmark(bufnr, ns_id, M.diff_tag)
+			M.diff_tag = nil
+		end
+
+		M.remove_keybind()
+		is_keybind_active = false
+	end
 
 	local function listener()
 		-- `nvim_win_get_cursor()` is 1-based; we subtract 1 to make it 0-based to match `target_line_number`
 		local current_line = vim.api.nvim_win_get_cursor(0)[1] - 1
 
 		if current_line == target_line_number and not is_keybind_active then
-			M.set_keybind()
-			is_keybind_active = true
+			entered_line()
 		elseif current_line ~= target_line_number and is_keybind_active then
-			M.remove_keybind()
-			is_keybind_active = false
+			exited_line()
 		end
+	end
+
+	-- See if user is already at the target line
+	if (vim.api.nvim_win_get_cursor(0)[1] - 1) == target_line_number and not is_keybind_active then
+		entered_line()
 	end
 
 	-- Watch for cursor movement in normal mode
