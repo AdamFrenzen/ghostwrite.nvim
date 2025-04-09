703c3fcf9319b44465213b6d6304a8a450e0244f
AdamFrenzen <adam.frenzen@gmail.com>
2025-04-08 21:15:07 -0600

    refactor(diff): move the highlights into their own file

---
+++ lua/ghostwrite/diff/highlights.lua
@@ -0,0 +1,31 @@
+-- Set highlight groups for diff visualization
+
+-- Green background for the suggested line
+vim.api.nvim_set_hl(0, "GhostwriteDiffGreen", {
+	fg = "#dbdbdb", -- light gray
+	bg = "#123a1b", -- green
+})
+-- Bright green background for emphasizing specific differences in the suggestion
+vim.api.nvim_set_hl(0, "GhostwriteDiffGreenBright", {
+	fg = "#dbdbdb", -- light gray
+	bg = "#156e2a", -- bright green
+})
+
+-- Red background for the original (current) line
+vim.api.nvim_set_hl(0, "GhostwriteDiffRed", {
+	-- Optional fg: remove to preserve original syntax highlighting
+	fg = "#dbdbdb", -- light gray
+	bg = "#450c0f", -- red
+})
+-- Bright red background for emphasizing specific differences in the original
+vim.api.nvim_set_hl(0, "GhostwriteDiffRedBright", {
+	-- Optional fg: remove to preserve original syntax highlighting
+	fg = "#dbdbdb", -- light gray
+	bg = "#6b1015", -- bright red
+})
+
+-- Purple background and gray text for the helper virtual line
+vim.api.nvim_set_hl(0, "GhostwriteDiffHelper", {
+	bg = "#3b245a", -- dark royal purple
+	fg = "#707070", -- dark gray
+})
